18ec078354b45a42606fddf44d40e52fdd8dc35a
Cavin-N-Hobees <43450959+Cavin-N-Hobees@users.noreply.github.com>
2018-10-25 18:33:04 -0600

    Removed comments and unused imports

--- src/Test2.java
+++ src/Test2.java
@@ -3,24 +3,20 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Test2 extends Application {
-  @Override // Override the start method in the Application class
-  public void start(Stage primaryStage) {    
-    // Create a pane to hold the circle 
+  @Override
+  public void start(Stage primaryStage) {     
     Pane pane = new Pane();
-    
-    // Create a circle and set its properties
     SpaceShip spaceship = new SpaceShip(new Image("Spaceship.png"),(pane.widthProperty().floatValue() / 2),pane.heightProperty().floatValue() / 2);
     //circle.xProperty().bind(pane.widthProperty().divide(2));
     //circle.yProperty().bind(pane.heightProperty().divide(2));
-    pane.getChildren().add(spaceship); // Add circle to the pane
+    pane.getChildren().add(spaceship);
+    spaceship.setScaleX(spaceship.getScaleX() * 2);
+    spaceship.setScaleY(spaceship.getScaleY() * 2);
     
     Timeline animation = new Timeline(
             new KeyFrame(Duration.millis(50), (e -> {
